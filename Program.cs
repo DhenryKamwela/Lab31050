95fdfa86786e649c238254673a545b27a4c6192c
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-11-16 12:41:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usrAns;
+            bool isTrue;
+            Console.WriteLine("True or False, the capital of Nigeria is Lagos?");
+            usrAns = Console.ReadLine();
+
+            if (usrAns == "True" || usrAns=="true")
+            {
+                isTrue = true;
+            }
+            else
+            {
+               isTrue = false;
+            }
+
+
+            if (isTrue)
+            {
+                Console.WriteLine("It is true.");
+            }
+            else
+            {
+                Console.WriteLine("It is false.");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
