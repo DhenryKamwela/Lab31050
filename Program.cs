1b0d29e3cdbf3b9e56087e6be8109034fdb2061a
DhenryKamwela <51858426+DhenryKamwela@users.noreply.github.com>
2019-12-01 11:43:34 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,39 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpeedLimit
+namespace WeatherLab2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a = 35;
-            int b = 42;
-            
-            int userANS;
-           
-            Console.WriteLine(" Enter your speed ");
-            userANS = Convert.ToInt32(Console.ReadLine());
-            if (userANS > b)
+            //if statement - Write a program that converts a Fahrenheit temperature to Celsius.
+            //The user should be able to input the temperature at the Command prompt and it should output the temperature.
+            //In addition, you should output “It is cold” if the Fahrenheit value is less that 40 or it should output 
+            //“It is hot” if the temperature is over 90 (4 points).
+            //Code to read a value: double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            //Code to convert: celsius  = (fahrenheit - 32d) * 5d / 9d;
+
+            double a = 40;
+            double b = 90;
+
+            Console.WriteLine("Enter your temperature in degrees Fahrenheit.");
+            double fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double celsius = (fahrenheit - 32d) * 5d / 9d;
+
+            if (fahrenheit > b)
             {
-                Console.WriteLine(" SLOW DOWN NOW! ");
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
+                Console.WriteLine("It is hot");
 
             }
-            else if (userANS > a)
+            else if (fahrenheit > a)
             {
-                Console.WriteLine(" Goodjob driving the speed limit ");
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
             }
             else
             {
-                Console.WriteLine("You need to speed up!");
+                Console.WriteLine("The tempurature in degrees Celsius is: " + celsius);
+                Console.WriteLine("It is cold");
             }
-                
+
             Console.ReadKey();
-
-              
-
-
-
 
 
 
